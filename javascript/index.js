4a0f5602071751b1b8b1281fcd72a31de227466f
sneha srishti <https://github.com/CodeBySnehaSrishti/MERN-Stack-Learning.git>
2026-03-14 15:17:55 -0800

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,76 @@
+// DATA TYPES
+// console.log(typeof(123))
+// console.log(typeof("sneha"))
+// console.log(typeof(true))
+
+// VARIABLES
+
+// var yourName=prompt("who the hell are you?")
+// alert("oh!, hello " + yourName)
+
+// STRING LENGTH
+
+// var yourName = "Sneha";
+// console.log(yourName.length)
+
+// COUNT LETTERS
+// var letters =prompt("letter limit is 50")
+// var lettersLenght =letters.length;
+// alert("you have written "+ lettersLenght +" letters and "+ (50-lettersLenght)+" left")
+
+//SlICING AND EXTRACTING PARTS OF A STRING
+
+// var tweet = prompt("write under 140 letters")
+// var letters140 = tweet.slice(0,140)
+// alert(letters140)
+
+// shortfom
+// alert(prompt("Compose your tweet").slice(0,140))
+
+// Changing Casing in Text
+
+// alert(prompt("enter your name").toUpperCase())
+
+// alert(prompt("enter your name").toLowerCase())
+
+// var yourName = prompt("enter your name")
+// var first = yourName.slice(0,1)
+// var second = yourName.slice(1,yourName.length)
+// var f =first.toUpperCase()
+// var s =second.toLowerCase()
+// alert("hello" +( f+s))
+
+// ARITHMETIC AND MODULO OPERATOR
+
+// alert(5%2)
+// alert(5/2)
+
+// Q. Dog age to human age formula
+
+// var dogAge=prompt("enter your dog age")
+// var age = ((dogAge - 2) * 4) + 21
+// alert("the age of your dog is "+dogAge+" and the human age is "+age)
+
+// INCREMENT AND DECREMENT EXPRESSIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
