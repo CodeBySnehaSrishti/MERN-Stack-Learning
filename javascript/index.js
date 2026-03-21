2d938c1c030adee6a1ddaf73cc812f3dec3c515e
sneha srishti <srishtisneha10@gmail.com>
2026-03-20 23:57:03 -0800

    add today progress

--- javascript/index.js
+++ javascript/index.js
@@ -304,6 +304,87 @@
 // }
 
 
+// WHO'S BUYING LUNCH
+
+// var names = ["sneha", "srishti", "kishu", "shriyansh", "shashwat", "megha"];
+// function whoPaying(names) {
+//     var numberOfPeople = names.length;
+//     var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+//     var randomPerson = names[randomPersonPosition];
+//     return randomPerson + " is going to buy lunch today!";
+// }
+// var result = whoPaying(names);
+// console.log(result);
+
+
+// FIZZBUZZ PROBLEM (white loop)
+
+// var output = [];
+// var count = 1;
+// function fizzBuzz() {
+//     while (count <= 20) {
+//         if (count % 3 === 0 && count % 5 === 0) {
+//             output.push("FizzBuzz");
+//         } else if (count % 3 === 0) {
+//             output.push("Fizz");
+//         } else if (count % 5 === 0) {
+//             output.push("Buzz");
+//         } else {
+//             output.push(count);
+//         }
+//         // count++;
+//     }
+//     console.log(output);
+// }
+
+// CONTROL STATEMENTS : FOR LOOPS 
+
+
+// var output = [];
+// function fizzBuzz() {
+//     for (var count = 1; count < 20, count++;) {
+//         if (count % 3 === 0 && count % 5 === 0) {
+//             output.push("FizzBuzz");
+//         } else if (count % 3 === 0) {
+//             output.push("Fizz");
+//         } else if (count % 5 === 0) {
+//             output.push("Buzz");
+//         } else {
+//             output.push(count);
+//         }
+//     }
+//     console.log(output);
+// }
+
+
+// FIBONACCI CHALLENGE
+
+// function fibonacciGenerator(n) {
+//     var output = [];
+//     if (n === 1) {
+//         output = [0];
+//     }
+//     else if (n === 2) {
+//         output = [0, 1];
+//     }
+//     else {
+//         output = [0, 1];
+//         for (var i = 2; i < n; i++){
+//             output.push(output[output.length - 2] + output[output.length - 1]);
+//     }
+// }
+// return output;
+// }
+// output =fibonacciGenerator(5);
+// console.log(output)
+
+
+
+
+
+
+
+
 
 
 
